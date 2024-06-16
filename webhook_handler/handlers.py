83d56bfe46adbb38712d3a5065a0d1a958551af3
Guhan Sensam <guhansensam@gmail.com>
2024-06-17 00:48:15 +0530

    Updated webhooks

--- webhook_handler/handlers.py
+++ webhook_handler/handlers.py
@@ -3,13 +3,11 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 import requests
 import json
-import os
 
 @receiver(post_save)
 # Webhook triggered when a new candidate is created
 def handle_model_update(sender, created, instance, **kwargs):
     if sender.__name__ == 'Candidate' and created: 
-        print("hello"*100)
         # send a request to the webhook
         url = "https://discord.com/api/webhooks/1214916560590741564/lWDmGMRLd7cZ8Aa7-819ewdAPqUPgse6mRPGRDbtJmNom3EgSeXrKX13TODMasqYZ1fs"
         data = {
@@ -22,9 +20,9 @@ def handle_model_update(sender, created, instance, **kwargs):
                         "url": "https://oxlac.com",
                         "icon_url": "https://oxlac.com/favicon.png"
                     },
-                    "title": "New Candidate has applied for position " + str(instance.job_position_id),
+                    "title": "New Candidate in " + str(instance.job_position_id),
                     "url": "https://employee.oxlac.com/candidate-view/" + str(instance.recruitment_id)+"/",
-                    "description": "Candidate Name: " + instance.name + "\n" + "Email: " + instance.email,
+                    "description": "Candidate Name: " + instance.name
                 }
             ]
         }
